5d74e74675f8a0efb5c75eb68565a3189ac1f9dc
Gourav Kumar Dhiman <gkrdhiman@gmail.com>
2023-08-19 14:16:33 +0530

    Update newgraph.c

--- DSA/Graph/newgraph.c
+++ DSA/Graph/newgraph.c
@@ -1,5 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
+struct Edge
+{
+    int data;
+    int weight;
+    struct Edge *next;
+};
+struct Graph{
+    int numvertices;
+    struct Edge* adj[100];
+};
+struct Edge *createEdge(int node,int weight){
+    struct Edge *newNode=(struct Edge *)malloc(sizeof(struct Edge));
+    newNode->data=node;
+    newNode->weight=weight;
+    newNode->next=NULL;
+    return newNode;
+}
+
+
 int main(){
+    struct Graph *graph;
     return 0;
 }
